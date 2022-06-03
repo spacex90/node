3b30ac98f2cf218c2fb8bae243fa1d2f175cf410
Sourav Das <itsmesourav101@gmail.com>
2022-06-03 18:31:45 +0530

    first commit

---
+++ module.js
@@ -0,0 +1,3 @@
+const myCalc = require('./Calc.js')
+
+console.log(myCalc.mul(3,2));
---
+++ server.js
@@ -0,0 +1,6 @@
+let http = require('http');
+
+http.createServer(function(req,res){
+    res.write("Welcome!");
+    res.end();
+}).listen(8080)
